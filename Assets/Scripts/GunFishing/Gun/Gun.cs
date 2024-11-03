617d01b1806934b3d7ca5bd3625f5cb20e8da02e
Alex <playstel.studio@gmail.com>
2024-11-03 20:14:28 +0100

    gun sync

--- Assets/Scripts/GunFishing/Gun/Gun.cs
+++ Assets/Scripts/GunFishing/Gun/Gun.cs
@@ -25,17 +25,34 @@ namespace GunFishing.Gun
         private const int CorrectionThreshold = 12;
         private const int MouseBounds = 7;
         
+        // Сетевые переменные для синхронизации
+        private NetworkVariable<Vector2> networkPosition = new NetworkVariable<Vector2>();
+        private NetworkVariable<bool> isShooting = new NetworkVariable<bool>();
+
+        
         public override void OnNetworkSpawn()
         {
-            // Этот код выполняется, когда объект появляется в сети
+            _transform = transform;
+            
             if (IsOwner)
             {
-                // Выполняется только для клиента, которому принадлежит объект
                 Debug.Log("OnNetworkSpawn as owner");
                 cameraMain = Camera.main;
-                _transform = transform;
             }
-            Debug.Log("OnNetworkSpawn");
+            else
+            {
+                // Если это не владелец, подписываемся на обновления сетевой позиции
+                networkPosition.OnValueChanged += OnNetworkPositionChanged;
+            }
+        }
+        
+        private void OnNetworkPositionChanged(Vector2 oldPosition, Vector2 newPosition)
+        {
+            // Обновляем локальную позицию, если это не владелец
+            if (!IsOwner)
+            {
+                _transform.position = newPosition;
+            }
         }
         
         private void Update()
@@ -45,34 +62,56 @@ namespace GunFishing.Gun
                 GunShoot();
                 GunMove();
             }
+            else
+            {
+                // Обновляем позицию для других клиентов
+                _transform.position = networkPosition.Value;
+            }
         }
 
         private void GunShoot()
         {
+            // if (Input.GetKeyDown(KeyCode.Space))
+            // {
+            //     _isAutomaticMode = !_isAutomaticMode;
+            //     UpdateFireModeUi();
+            // }
+            //
+            // if (_isAutomaticMode)
+            // {
+            //     if (Input.GetMouseButton(0) && Time.time >= _nextShotTime)
+            //     {
+            //         Shoot();
+            //         _nextShotTime = Time.time + (1f / fireRate); 
+            //     }
+            // }
+            // else
+            // {
+            //     if (Input.GetMouseButtonDown(0) && Time.time >= _nextShotTime)
+            //     {
+            //         Shoot();
+            //         _nextShotTime = Time.time + (1f / fireRate);
+            //     }
+            // }
+            
+            
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _isAutomaticMode = !_isAutomaticMode;
                 UpdateFireModeUi();
             }
 
-            if (_isAutomaticMode)
+            bool canShoot = _isAutomaticMode ? Input.GetMouseButton(0) : Input.GetMouseButtonDown(0);
+            
+            if (canShoot && Time.time >= _nextShotTime)
             {
-                if (Input.GetMouseButton(0) && Time.time >= _nextShotTime)
-                {
-                    Shoot();
-                    _nextShotTime = Time.time + (1f / fireRate); 
-                }
-            }
-            else
-            {
-                if (Input.GetMouseButtonDown(0) && Time.time >= _nextShotTime)
-                {
-                    Shoot();
-                    _nextShotTime = Time.time + (1f / fireRate);
-                }
+                _nextShotTime = Time.time + (1f / fireRate);
+
+                // Вызываем стрельбу на сервере, чтобы все клиенты увидели её
+                ShootServerRpc();
             }
         }
-
+        
         private void UpdateFireModeUi()
         {
             if (RoomInfoUi.Instance)
@@ -80,7 +119,65 @@ namespace GunFishing.Gun
                 RoomInfoUi.Instance.ChangeShootingMode("Fire mode: " + (_isAutomaticMode ? "Auto" : "Single") + " (press Space to change)");
             }
         }
+        
+        [ServerRpc]
+        private void ShootServerRpc()
+        {
+            // Сообщаем всем клиентам, что был произведен выстрел
+            ShootClientRpc();
+        }
+        
+        [ClientRpc]
+        private void ShootClientRpc()
+        {
+            // Выполняем локальную логику для всех клиентов
+            Shoot();
+        }
+        
 
+        private void Shoot()
+        {
+            var bulletInstance = GetObjectFromPool();
+
+            bulletInstance.TryGetComponent(out Bullet bullet);
+            {
+                bullet.SetHost(this);
+                SuccessRateCheck();
+            }
+        }
+        
+        private void SuccessRateCheck()
+        {
+            shotCount++;
+
+            if (shotCount >= CorrectionThreshold)
+            {
+                CheckSuccessRate();
+                shotCount = 0;
+                hitCount = 0;
+            }
+        }
+        
+        private GameObject GetObjectFromPool()
+        {
+            return ObjectPool.ObjectPool.Instance
+                .SpawnFromPool(bulletTag, _transform.position + Vector3.up, bulletPrefab.transform.rotation);
+        }
+
+        public void RegisterHit()
+        {
+            hitCount++;
+        }
+
+        private void CheckSuccessRate()
+        {
+            if (hitCount < 3)
+            {
+                Debug.Log("Making the game easier: spawn easy fish");
+                FishSpawner.Instance.SpawnEasyFish();
+            }
+        }
+        
         private void GunMove()
         {
             if (cameraMain == null)
@@ -96,53 +193,20 @@ namespace GunFishing.Gun
             
                 if (worldPosition.x is < MouseBounds and > -MouseBounds)
                 {
-                    _transform.position = new Vector2(worldPosition.x, PosY);
+                    var newPosition = new Vector2(worldPosition.x, PosY);
+                    _transform.position = newPosition;
+                    
+                    // Обновляем сетевую позицию на сервере
+                    UpdatePositionServerRpc(newPosition);
                 }
             }
         }
-
         
-        private void Shoot()
+        [ServerRpc]
+        private void UpdatePositionServerRpc(Vector2 newPosition)
         {
-            var bulletInstance = GetObjectFromPool();
-
-            bulletInstance.TryGetComponent(out Bullet bullet);
-            {
-                bullet.SetHost(this);
-                SuccessRateCheck();
-            }
-        }
-
-        private GameObject GetObjectFromPool()
-        {
-            return ObjectPool.ObjectPool.Instance
-                .SpawnFromPool(bulletTag, _transform.position + Vector3.up, bulletPrefab.transform.rotation);
-        }
-
-        private void SuccessRateCheck()
-        {
-            shotCount++;
-
-            if (shotCount >= CorrectionThreshold)
-            {
-                CheckSuccessRate();
-                shotCount = 0;
-                hitCount = 0;
-            }
-        }
-
-        public void RegisterHit()
-        {
-            hitCount++;
-        }
-
-        private void CheckSuccessRate()
-        {
-            if (hitCount < 3)
-            {
-                Debug.Log("Making the game easier: spawn easy fish");
-                FishSpawner.Instance.SpawnEasyFish();
-            }
+            // Обновляем сетевую позицию, которая синхронизируется с клиентами
+            networkPosition.Value = newPosition;
         }
     }
 }
